d01b785f7a9d7aaee487d19254570c8841c58736
amkhurana87 <81560418+amkhurana87@users.noreply.github.com>
2021-04-21 15:47:09 +0800

    add payload

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="App">
       <h1>{counter}</h1>
-      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(increment(9))}>+</button>
       <button>-</button>
     </div>
   );
